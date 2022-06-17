2b4ec14b1944b748c314f84d837712c09754de85
Ferreira Aguiar F Evangelista <b_ferreiraaguiarfeva@ats-hr420-dm11.local>
2022-06-17 13:19:43 -0400

    key

--- js/main.js
+++ js/main.js
@@ -2,5 +2,33 @@
 
 (() => {
     console.log('music player script file');
+
+
+
+    function logKeyboardKeyCode(event){
+        // event is an object that gets generated wherener the user presses a keyboard key -> it has a lot of info inside of it
+        
+        //find the audio element that matches the keyboard keycode
+
+        // query selector can take any valid css selector, inclusind attributes
+        
+        
+        let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+        if(targetAudio){
+            targetAudio.currentTime = 0;
+            targetAudio.play();
+        }else{
+            return;
+        }
+
+
+        debugger;
+    }
     
+
+
+    //add some event handling for keyboard events
+
+    window.addEventListener('keyup', logKeyboardKeyCode);
+
 })();
